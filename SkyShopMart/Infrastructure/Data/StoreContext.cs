0c33651d2fd67508c857338c7fff09ace78caab2
Rajasekarvscode <rajasekar10098@gmail.com>
2026-06-27 13:46:25 +0530

    Code changes entity

---
+++ SkyShopMart/Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Microsoft.Extensions.Options;
+using Infrastructure.Config;
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options) 
+    {
+       public DbSet<Product> products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfigurrtion).Assembly);
+        }
+    }
+}

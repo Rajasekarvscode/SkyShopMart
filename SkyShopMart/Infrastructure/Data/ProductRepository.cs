775be64ec9ad973df9bdd199d5baee37ab224681
Rajasekarvscode <rajasekar10098@gmail.com>
2026-06-27 23:49:31 +0530

    Code chages for Repo

---
+++ SkyShopMart/Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using Core.Entities;
+using Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository(StoreContext context) : IProductRepository
+    {
+        public void AddProduct(Product product)
+        {
+            context.SaveChangesAsync();
+        }
+
+        public void DeleteProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+           return await context.products.FindAsync(id);
+        }
+
+        public Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveProductAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SkyShopMart/SkyShop.API/Program.cs
+++ SkyShopMart/SkyShop.API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interface;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("SkyShopSystemDB"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
